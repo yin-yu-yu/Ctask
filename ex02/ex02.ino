0d2a8fb314ed36f558788db4a619e036bd3e9af0
Liu <qq1803528894@163.com>
2026-05-07 22:21:50 +0800

    作业2，判断逻辑改为更简洁的写法

--- ex02/ex02.ino
+++ ex02/ex02.ino
@@ -1,20 +1,12 @@
 const int ledPin = 4;
 const unsigned long intervalMs = 500;  // 500ms toggle -> 1Hz blink
 
-unsigned long previousMillis = 0;
-bool ledState = false;
-
 void setup() {
 	pinMode(ledPin, OUTPUT);
-	digitalWrite(ledPin, LOW);
 }
 
 void loop() {
 	unsigned long currentMillis = millis();
-
-	if (currentMillis - previousMillis >= intervalMs) {
-		previousMillis += intervalMs;
-		ledState = !ledState;
-		digitalWrite(ledPin, ledState ? HIGH : LOW);
-	}
+	bool ledState = (currentMillis / intervalMs) % 2 == 0;
+	digitalWrite(ledPin, ledState ? HIGH : LOW);
 }
